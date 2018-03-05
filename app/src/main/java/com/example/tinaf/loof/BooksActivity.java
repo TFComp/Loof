0ede13adbe8896b30661cc377df0257ca5a68b50
TFComp <tinafengwa@gmail.com>
2018-03-04 22:29:23 -0800

    aye

--- app/src/main/java/com/example/tinaf/loof/BooksActivity.java
+++ app/src/main/java/com/example/tinaf/loof/BooksActivity.java
@@ -2,17 +2,24 @@ package com.example.tinaf.loof;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.SparseBooleanArray;
 import android.view.View;
+import android.widget.AbsListView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import android.util.SparseBooleanArray;
 
-import static com.example.tinaf.loof.AnimeActivity.items;
+import static com.example.tinaf.loof.BooksActivity.items;
 
 public class BooksActivity extends AppCompatActivity {
+    ArrayList<String> arrayList;
+    ArrayAdapter<String> adapter;
+    static String[] items;
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +28,36 @@ public class BooksActivity extends AppCompatActivity {
         this.setTitle("Books");
         if (items == null) {
             items = new String[] {"one", "two", "three"};
+        }
+        listView = (ListView) findViewById(R.id.booksList);
+        arrayList = new ArrayList<>(Arrays.asList(items));
+        adapter = new ArrayAdapter<String>(this, R.layout.list_item, R.id.txtitem, arrayList);
+        listView.setAdapter(adapter);
+    }
+    public void onClick(View view) {
+        EditText textview = (EditText) findViewById(R.id.editText);
+        String editText = (String) textview.getText().toString();
+        arrayList.add(editText);
+        adapter.notifyDataSetChanged();
+    }
+    public void onDeleteClick(View view) {
+        EditText textview = (EditText) findViewById(R.id.editText);
+        String editText = (String) textview.getText().toString();
+        //arrayList.add(editText);
+        //adapter.notifyDataSetChanged();
+
+        //Log.i(TAG, "deleteSelectedEntries");
+
+        listView.setChoiceMode(AbsListView.CHOICE_MODE_SINGLE);
+
+        SparseBooleanArray checkedItems = listView.getCheckedItemPositions();
+        if (checkedItems != null) {
+            for (int i = 0; i < checkedItems.size(); ++i) {
+                if (checkedItems.valueAt(i)) {
+                    arrayList.remove(checkedItems.keyAt(i));
+                }
+                adapter.notifyDataSetChanged();
+            }
         }
     }
 }
