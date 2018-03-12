fde67b94f72487e40646328390323dda1e620931
TFComp <tinafengwa@gmail.com>
2018-03-11 20:46:26 -0700

    new

--- app/src/main/java/com/example/tinaf/loof/MangaActivity.java
+++ app/src/main/java/com/example/tinaf/loof/MangaActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.util.SparseBooleanArray;
 import android.view.View;
 import android.widget.AbsListView;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -34,20 +35,47 @@ public class MangaActivity extends AppCompatActivity {
         adapter = new ArrayAdapter<String>(this, R.layout.list_item, R.id.txtitem, arrayList);
         listView.setAdapter(adapter);
         listView.setChoiceMode(AbsListView.CHOICE_MODE_SINGLE);
+
+        listView.setClickable(true);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3){
+                Object o = listView.getItemAtPosition(position);
+                EditText name = (EditText) findViewById(R.id.name);
+                EditText description = (EditText) findViewById(R.id.description);
+                EditText episodNum = (EditText) findViewById(R.id.chapter);
+                name.setText(o.toString());
+                description.setText(o.toString());
+                episodNum.setText(o.toString());
+            }
+        });
     }
-    public void onClick(View view) {
+
+    public void onAddClick(View view) {
         EditText textview = (EditText) findViewById(R.id.name);
-        String editText = (String) textview.getText().toString();
-        arrayList.add(editText);
+        String name = (String) textview.getText().toString();
+        arrayList.add(name);
         adapter.notifyDataSetChanged();
     }
+
+    public void onUpdateClick(View view){
+        EditText textview = (EditText) findViewById(R.id.name);
+        String name = (String) textview.getText().toString();
+
+        SparseBooleanArray checkedItems = listView.getCheckedItemPositions();
+        if (checkedItems != null) {
+            for (int i = 0; i < checkedItems.size(); ++i) {
+                if (checkedItems.valueAt(i)) {
+                    arrayList.set(checkedItems.keyAt(i), name);
+                }
+                adapter.notifyDataSetChanged();
+            }
+        }
+    }
+
     public void onDeleteClick(View view) {
         EditText textview = (EditText) findViewById(R.id.name);
-        String editText = (String) textview.getText().toString();
-        //arrayList.add(editText);
-        //adapter.notifyDataSetChanged();
-
-        //Log.i(TAG, "deleteSelectedEntries");
+        String name = (String) textview.getText().toString();
 
         listView.setChoiceMode(AbsListView.CHOICE_MODE_SINGLE);
 
